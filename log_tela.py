7dee69a73a287e620b7448e6095c893445f20be7
AndrecastroSilva <167484644+Andregcs@users.noreply.github.com>
2025-04-02 10:07:20 -0300

    Commit inicial

---
+++ log_tela.py
@@ -0,0 +1,33 @@
+import tkinter as tk #importando a biblioteca - necessário em todos os arquivos que mexem na interface -.
+from tela_principal import TelaMenu #importando a classe da tela principal que chamei de menu. Podem mudar o nome, mas alterem as chamadas corretamente para não haver erros.
+
+class Tela: #iniciando a classe.
+    def __init__(self, master):
+        self.janela = master
+
+        self.janela.geometry("450x450") # definindo o tamanho da tela ao ser gerada. Meramente estética.
+
+        self.tela_principal = TelaMenu (self.janela) #tras a classe TelaMenu do arquivo 'tela_principal' para esta classe, ligando as duas .
+
+        self.log = tk.Frame(janela, width=200, height=300,) #inicio um frame pra comportar os elementos da tela de login. Usei por que acho que fica mais organizado, mas não é necessário.
+        self.log.place(relx=0.5, rely=0.5, anchor="center") #printando o frame. Usei o ".place" por sugestão do gpt. aparentemente ele só pode ser centralizado se usar esse place. 
+
+    # Adicionando elementos no frame:
+        self.lbl_user = tk.Label(self.log, text='Usuário:                                 ', font = ("Arial", 14)) # iniciei a label e usei essa quantidade de espaços pra ficar mais bonito visualmente.
+        self.lbl_user.grid(row=0, column=0, columnspan=2, sticky="w") # posicionando a label
+        self.user_ent = tk.Entry(self.log, bg='lightgray') # iniciando a entrada para receber a senha
+        self.user_ent.grid(row=1, column=0, columnspan=2, pady=5, ipady=5, ipadx=14, sticky='EW') # posicionando essa entrada
+        # TODOS OS COMPONENTES DAQUI PRA FENTE SEGUEM ESSE PADRÃO. CASO ALGO MUDE, EU COMENTO NA HORA.
+
+        self.lbl_senha = tk.Label(self.log, text = 'Senha:', font = ("Arial", 14))
+        self.lbl_senha.grid(row=2, column=0, sticky="w")
+        self.ent_senha = tk.Entry(self.log, bg='lightgray')
+        self.ent_senha.grid(row=3, column=0, columnspan=2, pady=5, ipady=5, ipadx=14, sticky='EW')
+
+        self.btn_entrar = tk.Button(self.log, text='Entrar', bg='darkblue', fg='white', font = ("Arial", 14), command=self.tela_principal.menu) #chama a classe TelaMenu a partir da interação com o botão.
+        self.btn_entrar.grid(row=4, column=0, columnspan=2, pady=10)
+
+# os comandos  abaixo fazem parte do loop da tela, que é usado pra iniciar a tela. N sei explicar, mas só sei fazer assim.
+janela = tk.Tk()
+app = Tela(janela)
+janela.mainloop()
---
+++ tela_principal.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import filedialog # biblioteca necessaria para a importação de arquivos
+
+class TelaMenu:
+    def __init__(self, janela):
+        self.janela = janela
+
+    def menu(self):
+        self.tpl_menu = tk.Toplevel(self.janela) # Inicia uma nova tela a partir do botão da tela de login.
+        self.tpl_menu.geometry('700x700')
+
+        self.mnu_principal = tk.Menu(self.tpl_menu) # criando uma barra de menu que vai receber elementos como inventario, e outros. Atenção: em caso de mudança, tomar cuidado pois o menu só aparece na tela quanado com os seus elementos, seja um ou mais.
+
+        # Adicionando o menu "Inventário" á barra:
+        self.mnu_inventario = tk.Menu(self.mnu_principal, tearoff=0) # gerando um "botão" na barra de menu
+        self.mnu_principal.add_cascade(label= 'Inventário', menu=self.mnu_inventario) # dando um nome e adicionando uma label a esse "botão"
+        self.mnu_inventario.add_command(label= 'Rotativo', command=self.interacao_inventario) # adicionando uma opção de interação que aparece ao clicar no "botão" do menu que serve como um comando para que quando clique nessa opção, apareça um frame que permite fazer upload de um arquivo
+
+        # Adicionando o menu "Ponto de vendas":
+        self.mnu_pt_vendas = tk.Menu(self.mnu_principal, tearoff=0)
+        self.mnu_principal.add_cascade(label = 'Ponto de Venda', menu=self.mnu_pt_vendas)
+        self.mnu_pt_vendas.add_command(label = 'Nova Venda')
+        self.mnu_pt_vendas.add_command(label ='Ver Vendas')
+
+        # adicionando o menu "Configurações" á barra:
+        self.mnu_configuracao = tk.Menu(self.mnu_principal, tearoff=0)
+        self.mnu_principal.add_cascade(label = 'Configurações')
+
+        self.tpl_menu.config(menu=self.mnu_principal) #Mostrando a barra do menu na tela.
+
+        # Frame do Inventário:
+        self.frm_inventario = tk.Frame(self.tpl_menu) # Criando o frame na tela tpl_menu. Ele está na tela menu, mas os componentes de dentro do frame apenas aparecem quando o usuário interagir com o menu.
+
+        self.frm_inventario.pack() # mostrando o frame na tela.
+
+    def interacao_inventario(self): # inicia uma função para mostrar o frame apenas ao interagir com o menu. Cada um dos elementos do menu precisa de um desses para serem independentes(até onde eu sei).
+        # as duas linhas abaixo verificam se é a primeira vez que o frame é chamado. se não for, ele destroi o frame anterior e tras um novo.
+        for widget in self.frm_inventario.winfo_children():
+            widget.destroy()
+        # Iniciando o frame de interação relativo ao menu Inventario:
+
+        
+        # Adicionando elementos no frame:
+        self.lbl_ad_arquivo = tk.Label(self.frm_inventario, text= 'Adicione o Arquivo') # label
+        self.lbl_ad_arquivo.pack() # mostrando a label
+
+        self.btn_ad_arquivo = tk.Button(self.frm_inventario, text= 'Upload', command= self.upp_arquivo) # botão com comando para carregar um arquivo da máquina
+        self.btn_ad_arquivo.pack() # mostrando o botão
+
+    def upp_arquivo(self):
+        caminho_arquivo = filedialog.askopenfilename(
+            title= "Selecione um Arquivo", 
+            filetypes= [("Todos os arquivos", "*.*"), ("Arquivos de texto", "*.txt"), ("Imagens", "*.png;*.jpg;*.jpeg")]
+        )
